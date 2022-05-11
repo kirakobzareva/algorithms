ebfa1ae6b0563517af9c29e3bdd0878aa48b1b26
Kira Kobzareva <kirakobzareva@gmail.com>
2022-05-11 12:27:36 +0200

    add string algorithm

---
+++ algorithms/leetCode/easy/final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,16 @@
+var finalValueAfterOperations = function (operations) {
+    let result = 0;
+
+    for (let i = 0; i < operations.length; i++) {
+        if (operations[i].includes("--")) {
+          result--;
+        } else {
+            result++;
+        }
+    }
+
+    return result;
+
+};
+
+console.log(finalValueAfterOperations(["--X", "X++", "X++"]));
